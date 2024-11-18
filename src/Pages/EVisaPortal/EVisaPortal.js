9a212ddd6f36b56112cda1db43219b5de9d74c76
Sakhawat Hosen <sakhawathosen320@gmail.com>
2024-11-18 12:04:06 +0600

    updted

--- src/Pages/EVisaPortal/EVisaPortal.js
+++ src/Pages/EVisaPortal/EVisaPortal.js
@@ -1,71 +1,156 @@
-import { Box, Button, Container, Divider, Grid, TextField, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Box,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  TextField,
+  Typography,
+  Alert,
+  Snackbar,
+} from "@mui/material";
+import React, { useState } from "react";
 import { RiVisaFill } from "react-icons/ri";
 import { IoIosArrowForward } from "react-icons/io";
 
 const EVisaPortal = () => {
-    return (
-        <Box>
+  const [visaID, setVisaID] = useState(""); // State to hold the input value
+  const [error, setError] = useState(""); // State to hold error messages
 
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // State for Snackbar visibility
 
-            {/* Main Content */}
-            <Container
-            >
-                <Box sx={{
-                    border: '1px solid #1D2D7A',
-                    borderRadius: '8px',
-                    height: "400px",
-                    px: "100px",
-                    py: "30px"
-                }}>
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-                    <Box sx={{ display: "flex", alignItems: "center", gap: "20px" }}>
-                        <Box sx={{ bgcolor: "#4064AE", height: "45px", width: "45px", borderRadius: "50%", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                            <Box>
-                                <RiVisaFill style={{ color: "white", fontSize: "25px" }} />
-                            </Box>
-                        </Box>
-                        <Box sx={{ borderBottom: "1px solid #4064AE", width: "100%", }}>
-                            <Typography sx={{ color: "#4064AE", fontSize: "22px" }}>Portal of Serbia e-Visa</Typography>
+    // Simple validation
+    if (!visaID) {
+      setError("e-Visa ID is required!");
+      setSnackbarOpen(true); // Open Snackbar
+      return;
+    }
 
+    // Simulate an API response
+    if (visaID) {
+      setError("Visa is not found!");
+      setSnackbarOpen(true); // Open Snackbar
+    } else {
+      setError(""); // Clear the error if the ID is correct
+      alert("Visa found successfully!");
+    }
+  };
 
-                        </Box>
-                    </Box>
-                    <Box sx={{ mt: "25px " }}>
-                        <form>
-                            <Box sx={{ display: "flex", flexDirection: "column" }}>
-                                <label style={{ color: "black", fontSize: "15px" }}>
-                                    <span style={{ color: "red", paddingRight: "5px" }}>*</span> e-Visa ID
-                                </label>
-                                <input placeholder='Enter ID here' style={{ paddingLeft: "10px", fontSize: "15px", width: "200px", marginTop: "10px", height: "26px", outline: "none", border: "1px solid grey", }} />
-                            </Box>
-                            <Box sx={{ display: "flex", alignItems: "center", justifyContent: "flex-end",mt:"50px" }}>
-                                <Button sx={{
-                                    background: "rgb(111 172 68)", color: "white", ":hover": {
-                                        background: "#FEFEFD", color: "#4064AE",
-                                        border: "1px solid #4064AE",
-                                    }
-                                    , textTransform: "capitalize",
-                                    borderRadius: "none", width: "150px",
-                                height:"40px"
-                                }}>
-                                    Confirm <IoIosArrowForward />
-                                </Button>
-                            </Box>
-
-                        </form>
-                    </Box>
-
-
-                </Box>
-
-
-
-
-            </Container>
-
+  // Function to close Snackbar
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+  return (
+    <Box>
+    {/* Main Content */}
+    <Container>
+      <Box
+        sx={{
+          border: "1px solid #1D2D7A",
+          borderRadius: "8px",
+          height: "400px",
+          px: "100px",
+          py: "30px",
+        }}
+      >
+        {/* Header Section */}
+        <Box sx={{ display: "flex", alignItems: "center", gap: "20px" }}>
+          <Box
+            sx={{
+              bgcolor: "#4064AE",
+              height: "45px",
+              width: "45px",
+              borderRadius: "50%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <RiVisaFill style={{ color: "white", fontSize: "25px" }} />
+          </Box>
+          <Box sx={{ borderBottom: "1px solid #4064AE", width: "100%" }}>
+            <Typography sx={{ color: "#4064AE", fontSize: "22px" }}>
+              Portal of Serbia e-Visa
+            </Typography>
+          </Box>
         </Box>
-    )
-}
 
-export default EVisaPortal
+        {/* Form Section */}
+        <Box sx={{ mt: "25px" }}>
+          <form onSubmit={handleSubmit}>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <label style={{ color: "black", fontSize: "15px" }}>
+                <span style={{ color: "red", paddingRight: "5px" }}>*</span>
+                e-Visa ID
+              </label>
+              <input
+                value={visaID}
+                onChange={(e) => setVisaID(e.target.value)} // Update visaID state
+                placeholder="Enter ID here"
+                style={{
+                  paddingLeft: "10px",
+                  fontSize: "15px",
+                  width: "200px",
+                  marginTop: "10px",
+                  height: "26px",
+                  outline: "none",
+                  border: "1px solid grey",
+                }}
+              />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                mt: "50px",
+              }}
+            >
+              <Button
+                type="submit"
+                sx={{
+                  background: "rgb(111 172 68)",
+                  color: "white",
+                  ":hover": {
+                    background: "#FEFEFD",
+                    color: "#4064AE",
+                    border: "1px solid #4064AE",
+                  },
+                  textTransform: "capitalize",
+                  borderRadius: "none",
+                  width: "150px",
+                  height: "40px",
+                }}
+              >
+                Confirm <IoIosArrowForward />
+              </Button>
+            </Box>
+          </form>
+        </Box>
+      </Box>
+    </Container>
+
+    {/* Snackbar Component */}
+    <Snackbar
+      open={snackbarOpen}
+      autoHideDuration={3000} // Automatically close after 3 seconds
+      onClose={handleCloseSnackbar}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+    >
+      <Alert
+        onClose={handleCloseSnackbar}
+        severity="error" // You can use "error", "success", "warning", or "info"
+        sx={{ width: "100%" }}
+      >
+        {error}
+      </Alert>
+    </Snackbar>
+  </Box>
+  );
+};
+
+export default EVisaPortal;
